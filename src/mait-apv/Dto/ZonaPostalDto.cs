90cfdc7c1769a3e269403cfae9bf2a48b0bae0b5
meliton <melitonio8@hotmail.com>
2025-06-29 22:24:04 +0100

    Agregar propiedad 'Centro' a ZonaPostalDto y actualizar datos en GQ.json

--- src/mait-apv/Dto/ZonaPostalDto.cs
+++ src/mait-apv/Dto/ZonaPostalDto.cs
@@ -6,6 +6,7 @@ public record ZonaPostalDto
     public string Distrito { get; init; } = string.Empty;
     public string Zona { get; init; } = string.Empty;
     public string Codigo { get; init; } = string.Empty;
+    public double[] Centro { get; init; } = [];
     public double[][] Poligono { get; init; } = [];
     public string Descripcion { get; init; } = string.Empty;
     public override string ToString() => $"{Codigo}, {Zona} - {Distrito}";
