54be710113dbc32c1b7a84d1838f46eff1b16e5c
Meliton Pablo Mangue <melitonio8@hotmail.com>
2025-07-15 16:10:09 +0100

    Ok

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 docker buildx create --use --name multiplatform-builder
 #docker buildx inspect --bootstrap
 
-docker buildx build ./src/mait-apv/ --platform linux/amd64 -t registry.mait.gq/apv:latest --push
+# docker buildx build ./src/mait-apv/ --platform linux/amd64 -t registry.mait.gq/apv:latest --push
+
+docker build --platform linux/amd64,linux/arm64 -t registry.mait.gq/apv:latest
+docker push registry.mait.gq/apv:latest
