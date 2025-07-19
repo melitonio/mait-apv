5767b58e55cf30ad8ac574c13a8231fc4f006652
Meliton Pablo Mangue <melitonio8@hotmail.com>
2025-07-19 17:16:26 +0100

    Actualiza las versiones de los paquetes MAIT.DataAccess y MAIT.Services a 2.10.164 en el archivo de proyecto.

--- src/mait-apv/Controllers/ApvController.cs
+++ src/mait-apv/Controllers/ApvController.cs
@@ -3,7 +3,6 @@ using Dto;
 using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Interfaces;
-using MAIT.Interfaces;
 using Services;
 
 namespace Controllers;
@@ -48,106 +47,6 @@ public partial class ApvController
             entity.CodigoPostal = zonaPostal?.Codigo ?? throw new($"No se ha encontrado la zona postal con el código: {entity.CodigoPostal}");
         }
         return base.OnUpdateAsync(entity, dto);
-    }
-
-
-    [HttpPost("{id}/active")]
-    [ProducesResponseType(typeof(ResultModel<bool>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ResultModel<bool>> Activate(Guid id)
-    {
-        try
-        {
-            var entity = await _crudService.GetByIdAsync(id);
-            if (entity == default)
-            {
-                Response.StatusCode = StatusCodes.Status404NotFound;
-                _logger.LogWarning("No se ha encontrado el apartado con ID: {Id}", id);
-                return new($"No se ha encontrado el apartado con ID: {id}");
-            }
-            if (entity.IsActive)
-            {
-                Response.StatusCode = StatusCodes.Status400BadRequest;
-                _logger.LogWarning("El apartado con ID: {Id} ya está activo.", id);
-                return new($"El apartado con ID: {id} ya está activo.");
-            }
-            entity.Activate(GetUsername());
-            await _crudService.UpdateItemAsync(entity);
-            return new(true);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error al activar el apartado con ID: {Id}", id);
-            Response.StatusCode = StatusCodes.Status400BadRequest;
-            return new("Error al activar el apartado");
-        }
-    }
-
-
-    [HttpPost("{id}/suspend")]
-    [ProducesResponseType(typeof(ResultModel<bool>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ResultModel<bool>> Suspend(Guid id)
-    {
-        try
-        {
-            var entity = await _crudService.GetByIdAsync(id);
-            if (entity == default)
-            {
-                Response.StatusCode = StatusCodes.Status404NotFound;
-                _logger.LogWarning("No se ha encontrado el apartado con ID: {Id}", id);
-                return new($"No se ha encontrado el apartado con ID: {id}");
-            }
-            if (entity.IsSuspended)
-            {
-                Response.StatusCode = StatusCodes.Status400BadRequest;
-                _logger.LogWarning("El apartado con ID: {Id} ya está suspendido", id);
-                return new($"El apartado con ID: {id} ya está suspendido");
-            }
-            entity.Suspend(GetUsername());
-            await _crudService.UpdateItemAsync(entity);
-            return new(true);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error al suspender el apartado con ID: {Id}", id);
-            Response.StatusCode = StatusCodes.Status400BadRequest;
-            return new("Error al suspender el apartado");
-        }
-    }
-
-
-    [HttpPost("{id}/aprove")]
-    [ProducesResponseType(typeof(ResultModel<bool>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ResultModel<bool>> Aprove(Guid id)
-    {
-        try
-        {
-            var entity = await _crudService.GetByIdAsync(id);
-            if (entity == default)
-            {
-                Response.StatusCode = StatusCodes.Status404NotFound;
-                _logger.LogWarning("No se ha encontrado el apartado con ID: {Id}", id);
-                return new($"No se ha encontrado el apartado con ID: {id}");
-            }
-
-            if (entity.IsApproved == true)
-            {
-                Response.StatusCode = StatusCodes.Status400BadRequest;
-                _logger.LogWarning("El apartado con ID: {Id} ya está aprobado", id);
-                return new($"El apartado con ID: {id} ya está aprobado");
-            }
-            entity.Approve(GetUsername());
-            await _crudService.UpdateItemAsync(entity);
-            return new(true);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error al aprobar el apartado con ID: {Id}", id);
-            Response.StatusCode = StatusCodes.Status400BadRequest;
-            return new("Error al aprobar el apartado");
-        }
     }
 
 }
